24e45ff3f6725b9952381e8cd2dd6b53bf2f1b0b
Jan Poonthong <janpoonthong628@gmail.com>
2025-02-03 19:18:33 +0700

    feat: bottom navigation

--- app/src/main/java/com/example/auevent/MainActivity.kt
+++ app/src/main/java/com/example/auevent/MainActivity.kt
@@ -3,26 +3,68 @@ package com.example.auevent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.DateRange
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.auevent.pages.CreatePage
+import com.example.auevent.pages.EventPage
+import com.example.auevent.pages.HomePage
 import com.example.auevent.ui.theme.AUEventTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             AUEventTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                val items = listOf(
+                    BottomNavigationItem("Home", Icons.Filled.Home, Icons.Outlined.Home),
+                    BottomNavigationItem("Create", Icons.Filled.Add, Icons.Outlined.Add),
+                    BottomNavigationItem("Event", Icons.Filled.DateRange, Icons.Outlined.DateRange)
+                )
+                var selectedItemIndex by rememberSaveable { mutableIntStateOf(0) }
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    bottomBar = {
+                        NavigationBar {
+                            items.forEachIndexed { index, item ->
+                                NavigationBarItem(
+                                    selected = selectedItemIndex == index,
+                                    onClick = { selectedItemIndex = index },
+                                    label = { Text(text = item.title) },
+                                    icon = {
+                                        Icon(
+                                            imageVector = if (selectedItemIndex == index) item.selectedIcon else item.unselectedIcon,
+                                            contentDescription = item.title
+                                        )
+                                    }
+                                )
+                            }
+                        }
+                    }
+                ) { innerPadding ->
+                    ContentScreen(
+                        modifier = Modifier.padding(innerPadding),
+                        selectedItemIndex
                     )
                 }
             }
@@ -31,17 +73,10 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AUEventTheme {
-        Greeting("Android")
+fun ContentScreen(modifier: Modifier = Modifier, selectedItemIndex: Int) {
+    when (selectedItemIndex) {
+        0 -> HomePage()
+        1 -> CreatePage()
+        2 -> EventPage()
     }
 }
---
+++ app/src/main/java/com/example/auevent/pages/HomePage.kt
@@ -0,0 +1,25 @@
+package com.example.auevent.pages
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun HomePage(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        Text(
+            text = "Home Page",
+            fontSize = 40.sp,
+            fontWeight = FontWeight.SemiBold,
+        )
+    }
+}
